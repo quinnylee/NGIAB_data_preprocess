605f364df2c9d2f7c1aefb2564b962a0141cebda
Quinn Lee <qylee@ua.edu>
2025-10-17 07:24:18 -0500

    removed print statements

--- modules/data_processing/forcings.py
+++ modules/data_processing/forcings.py
@@ -155,7 +155,6 @@ def add_pet_to_dataset(dataset: xr.Dataset) -> xr.Dataset:
         pet = np.zeros(tmin.shape, dtype=np.float32) * np.nan
 
         temp_range = tmax - tmin
-        print("temp-range", temp_range.shape)
         temp_range[temp_range < 0] = 0
 
         latitude = np.deg2rad(lat)
@@ -167,11 +166,9 @@ def add_pet_to_dataset(dataset: xr.Dataset) -> xr.Dataset:
         tmp3 = np.cos(latitude) * np.cos(sol_dec) * np.sin(sha)
         et_rad = tmp1 * SOLAR_CONSTANT * ird * (tmp2 + tmp3)
         et_rad = et_rad.reshape(-1)
-        print("et rad", et_rad.shape)
         pet = 0.0023 * (tmean + 17.8) * temp_range ** 0.5 * 0.408 * et_rad
 
         pet[pet < 0] = 0
-        print(pet.shape)
         return pet
     
     # read 24 hour chunks at a time to calculate temperature stats
@@ -226,15 +223,12 @@ def add_pet_to_dataset(dataset: xr.Dataset) -> xr.Dataset:
         cat_temps = day_chunk.values
         # calculate stats
         tmin = np.min(cat_temps, axis=1)
-        print(tmin.shape)
         tmax = np.max(cat_temps, axis=1)
         tmean = np.mean(cat_temps, axis=1)
         lat = dataset['lat'].values
 
         pet = hargreaves(tmin, tmax, tmean, lat, time_range)
-        # print(pet.shape)
         day_pet = np.repeat(pet[:, np.newaxis], ts_diff, axis=1)
-        # print(day_pet.shape)
         pet_array[:, day_chunk_start_idx:day_chunk_start_idx+ts_diff] = day_pet
 
         day_chunk_start_idx += 24
